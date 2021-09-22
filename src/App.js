54358bd39dcaa9189c421f9996b41568362e568b
Clay Stewart <claytonmstewart@gmail.com>
2021-09-22 14:53:25 -0500

    add source code from other versions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled from "@emotion/styled";
+import {
+  calcFatProteinUnit,
+  calcCKal,
+  calcCarbConversion,
+  calcInsulinDose,
+  calcDuration,
+} from "./business-logic";
+
+const AppWrapper = styled.div({
+  color: "#FFF",
+  backgroundColor: "#000",
+  position: "absolute",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+});
+
+const Header = styled.div({
+  fontSize: 24,
+});
+
+const InputNumber = styled.input({
+  height: 40,
+  margin: 12,
+  borderWidth: 1,
+  padding: 10,
+  fontSize: 24,
+});
+
+const OutputText = styled.div({
+  fontSize: 24,
+  borderBottom: "1px solid white",
+  padding: "10px 0",
+});
 
 function App() {
+  const [fat, setFat] = useState(20);
+  const [protein, setProtein] = useState(30);
+  const [icr, setIcr] = useState(30);
+
+  const fatProteinUnit = calcFatProteinUnit(fat, protein);
+  const ckal = calcCKal(fatProteinUnit);
+  const carbConversion = calcCarbConversion(fatProteinUnit);
+  const insulinDose = calcInsulinDose(icr, carbConversion);
+  const duration = calcDuration(ckal);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      <Header>Fat protein dose</Header>
+      <div>Fat: {fat}</div>
+      <div>
+        <InputNumber
+          type="number"
+          defaultValue={fat}
+          onChange={(e) => setFat(e.target.value)}
+        />
+      </div>
+      <div>Protein: {protein}</div>
+      <div>
+        <InputNumber
+          type="number"
+          defaultValue={protein}
+          onChange={(e) => setProtein(e.target.value)}
+        />
+      </div>
+      <div>ICR: 1 unit per {icr}</div>
+      <div>
+        <InputNumber
+          type="number"
+          defaultValue={fat}
+          onChange={(e) => setIcr(e.target.value)}
+        />
+      </div>
+
+      <OutputText>FPU: {fatProteinUnit}</OutputText>
+      <OutputText>ckal: {ckal}</OutputText>
+      <OutputText>Carb Convertion: {carbConversion}</OutputText>
+      <OutputText>Insulin Dose: {insulinDose}</OutputText>
+      <OutputText>Duration: {duration}</OutputText>
+    </AppWrapper>
   );
 }
 
---
+++ src/business-logic.js
@@ -0,0 +1,96 @@
+/*
+Terms:
+* ICR is the grams of carbohydrate counteracted by 1 unit of rapid-acting insulin
+* FPU: fat–protein unit
+ */
+
+// [B5] =IF(B1>0,((B1*9)+(B2*4)),"")
+export const calcFatProteinUnit = (fat, protein) => {
+  if (fat <= 0) return "";
+
+  return fat * 9 + protein * 4;
+}
+
+// [B7] =ARRAY_CONSTRAIN(
+//         ARRAYFORMULA(
+//           IFS(
+//             B5="","",
+//             B5>0,
+//             ROUND(B5/100,1)
+//           )
+//         ), 1, 1)
+export const calcCKal = (fatProteinUnit) => {
+  if (fatProteinUnit === "") return "";
+
+  if (fatProteinUnit > 0) {
+    // needs to round to 1 decimal place
+    return Math.round(fatProteinUnit / 100).toFixed(1);
+  }
+
+  return "";
+};
+
+// [B9] =ARRAY_CONSTRAIN(
+//         ARRAYFORMULA(
+//           IFS(
+//             B5="","",
+//             B5>0,ROUND(B5/10,0)
+//           )
+//         ), 1, 1)
+export const calcCarbConversion = (fatProteinUnit) => {
+  if (fatProteinUnit === "") return "";
+
+  if (fatProteinUnit > 0) {
+    // needs to round to 0 decimal places
+    return Math.round(fatProteinUnit / 10).toFixed();
+  }
+
+  return "";
+};
+
+// [B11] ==ARRAY_CONSTRAIN(
+//           ARRAYFORMULA(
+//             IFS(
+//               AND(B3="",B9=""),"",
+//               AND(B3>0,B9>0),
+//                 (
+//                   ROUND(B9/B3,1)
+//                 )
+//               )
+//           ), 1, 1)
+export const calcInsulinDose = (irc, carbConversion) => {
+  if (irc === "" || carbConversion === "") return "";
+
+  if (irc > 0 && carbConversion > 0) {
+    // needs to round to 1 decimal place
+    return Math.round(carbConversion / irc).toFixed(1);
+  }
+
+  return "";
+};
+
+// [B13] =ARRAY_CONSTRAIN(
+//          ARRAYFORMULA(
+//            IFS(
+//              B7="","",
+//              B7>4,"8",
+//              (AND(
+//                B7>=3,B7<4)),"5",
+//              (AND(
+//                B7>=2,B7<3)),"4",
+//              (AND(
+//                B7>=1,B7<2)),"3")
+//            ), 1, 1)
+export const calcDuration = (cKal) => {
+  if (cKal === "") return "";
+
+  if (cKal >= 4) return 8;
+
+  if (cKal >= 3 && cKal < 4) return 5;
+
+  if (cKal >= 2 && cKal < 3) return 4;
+
+  if (cKal >= 1 && cKal < 2) return 3;
+
+  return "";
+};
